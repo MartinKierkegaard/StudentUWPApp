71f0eb8b3533faa055c651ca0bb1d45dbe3cb9d4
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-04-20 10:48:55 +0200

    Add project files.

---
+++ StudentUWPApp/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentUWPApp
+{
+    public class Student: INotifyPropertyChanged
+	{
+		private string _navn;
+
+		public string Navn
+		{
+			get { return _navn; }
+			set { _navn = value;
+				this.OnPropertyChanged();
+			}
+		}
+
+		private int _studentID;
+
+		public int StudentID
+		{
+			get { return _studentID; }
+			set { _studentID = value;
+				this.OnPropertyChanged();
+			}
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+	}
+}
